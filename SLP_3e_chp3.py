c82fb07e2face8709f02288b93bc3a19dec6c234
Vaibhav <vaibhav.mittal199696@gmail.com>
2021-09-19 17:57:42 +0530

    Added first files

---
+++ SLP_3e_chp3.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # N-grams
+
+# In[ ]:
+
+
+import nltk
+nltk.download()
+# download nltk.book
+
+
+# In[1]:
+
+
+from nltk.book import *
+
+
+# In[5]:
+
+
+text1.concordance("""its water is so transparent that""")
+
+
+# In[ ]:
+
+
+
+
